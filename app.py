1e5bd2c32bcfab10d3642fd3d7dc1f9a6ff199b2
Mate Ajdukovic <mate.ajdukovic@gmail.com>
2020-05-03 17:11:06 +0200

    Added run.py file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
 
-from db import db
 from resources.item import Item, ItemList
 from resources.user import UserRegister
 from security import authenicate, identity
@@ -13,10 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICTIONS'] = False
 app.secret_key = 'mate'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenicate, identity) # /auth -> endpoint created by JWT
 
@@ -29,5 +24,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
